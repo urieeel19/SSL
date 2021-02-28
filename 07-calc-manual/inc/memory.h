85f73359532d5c4658ac2d69c7fa474285d9e3a1
urieeel19 <urielrubin03@hotmail.com>
2021-02-28 02:26:56 -0300

    Refactor

--- 07-calc-manual/inc/memory.h
+++ 07-calc-manual/inc/memory.h
@@ -1,25 +1,18 @@
 #define MEMORY_SIZE 200
-#define VARIABLE_SIZE 10
 
 #ifndef MEMORY_H
 #define MEMORY_H
 
-// Declaracion de la memoria
-//extern int memoryLastPosition;
-struct variable
+// Definición de la estructura variable
+typedef struct variable
 {
-    char name[VARIABLE_SIZE];
+    char name[10];
     int value;
-};
-
-typedef struct variable variable;
+} variable;
 
 #endif
 
-//FUNCIONES PARA MANEJO DE MEMORIA
+// Prototipos de funciones públicas para el manejo de memoria.
 unsigned GetPosition(char[]);
 void Assign(unsigned, int);
 int GetValue(char[]);
-
-//FUNCIONES PARA DEBUG
-void mostrarMemoria();
--- 07-calc-manual/inc/scanner.h
+++ 07-calc-manual/inc/scanner.h
@@ -1,36 +1,42 @@
-#define MAX_NAME_LENGTH 10
+#define MAX_NAME_LENGTH 20
 
 #ifndef SCANNER_H
 #define SCANNER_H
 
-// DEFINICION DE TIPO DE TOKEN
+// Declaración de flag para habilitar/deshabilitar consumo de TOKENS
+extern int keepLastToken;
+
+// Definición tipoDeToken
 typedef enum
 {
     NAT,
+    INICIO,
+    FIN,
     IDENTIFICADOR,
     CONSTANTE,
     IGUAL,
     PARENIZQUIERDO,
     PARENDERECHO,
     SUMA,
-    MULTIPLICACION,
-    DEF,
+    PRODUCTO,
+    DEFINICION,
     FDS,
     FDT
 } tipoDeToken;
 
-struct TOKEN
+// Definición de la estructura TOKEN
+typedef struct TOKEN
 {
-    tipoDeToken type;
-    union
+    tipoDeToken type;   //Tipo
+    union               //string o int
     {
-        char name[10]; //string
-        int value;     //int o double
+        char name[MAX_NAME_LENGTH]; 
+        int value;     
     } data;
-};
-
-typedef struct TOKEN TOKEN;
+} TOKEN;
 
 #endif
 
+// Prototipo de funciones públicas
+// Función que se encarga de devolver los TOKENs según lo que el scanner analiza.
 TOKEN GetNextToken(void);
